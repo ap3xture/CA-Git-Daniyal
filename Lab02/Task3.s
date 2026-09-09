3ef141d6590f46f7e810130031db6a2218d7821d
Dan <ap3xture@gmail.com>
2026-09-09 18:14:15 +0500

    task3 done

--- Lab02/Task3.s
+++ Lab02/Task3.s
@@ -9,34 +9,9 @@ main:
 loop:
     bge x22, x24, end # end if false
 
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    sb x22,0x0(x6)  # set a[i] =i
-
-
-
-
-    addi x22, x22, 1    # i++
-    j loop              # back to loop
-end:
-
-main1:
-    li x22, 0   #i
-    li x23, 0   #sum# i = x22, array_a_base = x5,
-# sum = x23, max = 10 = x24
-
-main:
-    li x22, 0   #i
-    li x23, 0   #sum
-    li x24, 10 # 10
-    li x5, 0x200 # array base
-loop:
-    bge x22, x24, end # end if false
-
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    sb x22,0x0(x6)  # set a[i] =i
-
-
-
+    slli x6, x22, 2
+    add x6, x5, x6 # 0x200 .. 0x204 ... 0x208
+    sw x22, 0x0(x6)  # set a[i] =i
 
     addi x22, x22, 1    # i++
     j loop              # back to loop
@@ -50,60 +25,12 @@ main1:
 loop1:
     bge x22, x24, end1 # end if false
 
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    lb x25,0x0(x6)  # put a[i in x25
-    add x23,x23, x25 # sum += a[i
-
-
-    addi x22, x22, 1    # i++
-    j loop1              # back to loop
-end1:# i = x22, array_a_base = x5,
-# sum = x23, max = 10 = x24
-
-main:
-    li x22, 0   #i
-    li x23, 0   #sum
-    li x24, 10 # 10
-    li x5, 0x200 # array base
-loop:
-    bge x22, x24, end # end if false
-
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    sb x22,0x0(x6)  # set a[i] =i
-
-
-
-
-    addi x22, x22, 1    # i++
-    j loop              # back to loop
-end:
-
-main1:
-    li x22, 0   #i
-    li x23, 0   #sum
-    li x24, 10 # 10
-    li x5, 0x200 # array base
-loop1:
-    bge x22, x24, end1 # end if false
-
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    lb x25,0x0(x6)  # put a[i in x25
-    add x23,x23, x25 # sum += a[i
-
+    slli x6, x22, 2
+    add x6, x5, x6 # 0x200 .. 0x204 ... 0x208
+    lw x25, 0x0(x6)  # put a[i in x25
+    add x23, x23, x25 # sum += a[i
 
     addi x22, x22, 1    # i++
     j loop1              # back to loop
 end1:
-    li x24, 10 # 10
-    li x5, 0x200 # array base
-loop1:
-    bge x22, x24, end1 # end if false
-
-    add x6,x5, x22 # 0x200 .. 0x201 ... 0x202
-    lb x25,0x0(x6)  # put a[i in x25
-    add x23,x23, x25 # sum += a[i
-
-
-    addi x22, x22, 1    # i++
-    j loop1              # back to loop
-end1:
+    j end1
